83ea1ccf4bb92ce5397718a13ee32b5437fe911e
NeverZoom <mrneverzoom@gmail.com>
2018-11-28 18:14:24 +0300

    LoginActivity created Add animations and btn styles

--- app/src/main/java/com/example/kiri4/instwtf/LoginActivity.kt
+++ app/src/main/java/com/example/kiri4/instwtf/LoginActivity.kt
@@ -1,17 +1,21 @@
 package com.example.kiri4.instwtf
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 
-class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, TextWatcher {
+class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, TextWatcher, View.OnClickListener {
     private val TAG = "LoginActivity"
+    private lateinit var nAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,24 @@ class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, Text
         login_btn.isEnabled = false
         email_input.addTextChangedListener(this)
         password_input.addTextChangedListener(this)
+        login_btn.setOnClickListener(this)
+
+        nAuth = FirebaseAuth.getInstance()
+    }
+
+    override fun onClick(view: View) {
+        val email = email_input.text.toString()
+        val password = password_input.text.toString()
+        if(validate(email, password)) {
+            nAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if(it.isSuccessful) {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    finish()
+                }
+            }
+        } else {
+            Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onVisibilityChanged(isKeyboardOpen: Boolean) {
@@ -35,10 +57,12 @@ class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, Text
     }
 
     override fun afterTextChanged(s: Editable?) {
-        login_btn.isEnabled =
-                email_input.text.toString().isNotEmpty() &&
-                password_input.text.toString().isNotEmpty()
+        login_btn.isEnabled = validate(email_input.text.toString(), password_input.text.toString())
     }
+
+    private fun validate(email: String, password: String) =
+        email.isNotEmpty() && password.isNotEmpty()
+
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
